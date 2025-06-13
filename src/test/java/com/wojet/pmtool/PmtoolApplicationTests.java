0c32b6d3870e96fefec6a28c5921e1429ed76cd2
workualemu <alemu.worku@gmail.com>
2025-06-13 16:44:09 -0400

    First tests context load and client.save()

--- src/test/java/com/wojet/pmtool/PmtoolApplicationTests.java
+++ src/test/java/com/wojet/pmtool/PmtoolApplicationTests.java
@@ -2,14 +2,31 @@ package com.wojet.pmtool;
 
 // import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.wojet.pmtool.model.Client;
+import com.wojet.pmtool.repository.ClientRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-// @Disabled("Disabled until CI is fully configured")
 class PmtoolApplicationTests {
+
+	@Autowired
+	ClientRepository clientRepository;
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isTrue();
+	}
+
+	@Test
+	void canSaveClient() {
+		Client client = new Client();
+		client.setName("Test Client");
+		clientRepository.save(client);
+		assertThat(client.getId()).isNotNull();
 	}
 
 }
